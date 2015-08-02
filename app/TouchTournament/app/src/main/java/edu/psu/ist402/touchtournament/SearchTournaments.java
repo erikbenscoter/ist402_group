503853e587e61633ce70ceab0bc522ea956f08aa
dbecker72 <dbecker72@gmail.com>
2015-08-02 14:52:23 -0400

    Existing Tournament UI created

--- app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/SearchTournaments.java
+++ app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/SearchTournaments.java
@@ -1,25 +1,23 @@
 package edu.psu.ist402.touchtournament;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SearchTournaments extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search_tournaments);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_search_tournaments, menu);
         return true;
     }
 
@@ -36,26 +34,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void createNewTournament(View view) {
-        Intent intent = new Intent (this, CreateNewTourney.class);
-        startActivity(intent);
-    }
-
-    public void runExisting(View view) {
-        Intent intent = new Intent (this, SelectExistingTournament.class);
-        startActivity(intent);
-    }
-
-    public void viewInProgres(View view) {
-        Intent intent = new Intent (this, SelectExistingTournament.class);
-        startActivity(intent);
-    }
-
-    public void searchCompleted(View view) {
-        Intent intent = new Intent (this, SearchTournaments.class);
-        startActivity(intent);
-
     }
 }
--- app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/SelectExistingTournament.java
+++ app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/SelectExistingTournament.java
@@ -6,12 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class ExistingTournament extends ActionBarActivity {
+public class SelectExistingTournament extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_existing_tournament);
+        setContentView(R.layout.activity_select_existing_tournament);
     }
 
     @Override
