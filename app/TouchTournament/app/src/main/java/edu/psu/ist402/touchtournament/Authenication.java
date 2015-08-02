f5853be4d3cf3b88db0bb5ce36ee85aa1de84802
dbecker72 <dbecker72@gmail.com>
2015-08-02 15:22:20 -0400

    Created Authenication UI

--- app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/Authenication.java
+++ app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/Authenication.java
@@ -1,25 +1,23 @@
 package edu.psu.ist402.touchtournament;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class SelectExistingTournament extends ActionBarActivity {
+public class Authenication extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_existing_tournament);
+        setContentView(R.layout.activity_authenication);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_open_existing_tournament, menu);
+        getMenuInflater().inflate(R.menu.menu_authenication, menu);
         return true;
     }
 
@@ -36,10 +34,5 @@ public class SelectExistingTournament extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void select(View view) {
-        Intent intent = new Intent (this, Authenication.class);
-        startActivity(intent);
     }
 }
