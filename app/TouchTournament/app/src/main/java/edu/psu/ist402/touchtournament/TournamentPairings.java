dea9b05d91dc91a79c4d9bb699a796f7e4637a09
dbecker72 <dbecker72@gmail.com>
2015-08-02 16:25:28 -0400

    Created Authenication UI

---
+++ app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/TournamentPairings.java
@@ -0,0 +1,45 @@
+package edu.psu.ist402.touchtournament;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TableLayout;
+
+
+public class TournamentPairings extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tournament_pairings);
+
+
+        TableLayout t1 = (TableLayout) findViewById(R.id.tourneyTableLayout);
+        t1.addView(row1);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tournament_pairings, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
