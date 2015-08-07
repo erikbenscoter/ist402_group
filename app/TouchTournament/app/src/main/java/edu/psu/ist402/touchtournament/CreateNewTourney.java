024507c5eca5d0a7c43ffe236e6256e87376c709
dbecker72 <dbecker72@gmail.com>
2015-08-06 22:07:24 -0400

    Date picker updated on create tourney page

--- app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/CreateNewTourney.java
+++ app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/CreateNewTourney.java
@@ -1,24 +1,90 @@
 package edu.psu.ist402.touchtournament;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
-import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 
 public class CreateNewTourney extends ActionBarActivity {
+
+    EditText TextViewBeginningDate;
+    EditText TextViewEndingDate;
+
+    Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener BeginDate = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateTextViewBeginningDate();
+        }
+
+    };
+
+    DatePickerDialog.OnDateSetListener EndDate = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateTextViewEndingDate();
+        }
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_tourney);
+
+        TextViewBeginningDate = (EditText) findViewById(R.id.TextViewBeginningDate);
+        TextViewEndingDate = (EditText) findViewById(R.id.TextViewEndingDate);
+
+        TextViewBeginningDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(CreateNewTourney.this, BeginDate, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        TextViewEndingDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(CreateNewTourney.this, EndDate, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -73,7 +139,7 @@ public class CreateNewTourney extends ActionBarActivity {
         if( !DatabaseCommunicator.IsLegitimate(m_email,m_password) ){
             Toast.makeText(getApplicationContext(),"That is not a legitimate username/password combo",Toast.LENGTH_LONG).show();
             return;
-        };
+        }
 
 
         //create query to insert into Tournament Table
@@ -132,4 +198,25 @@ public class CreateNewTourney extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void updateTextViewBeginningDate() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        TextViewBeginningDate.setText(sdf.format(myCalendar.getTime()));
+
+
+    }
+
+    private void updateTextViewEndingDate() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        TextViewEndingDate.setText(sdf.format(myCalendar.getTime()));
+
+
+    }
+
 }
--- app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/EnterTeamData.java
+++ app/TouchTournament/app/src/main/java/edu/psu/ist402/touchtournament/EnterTeamData.java
@@ -1,9 +1,11 @@
 package edu.psu.ist402.touchtournament;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class EnterTeamData extends ActionBarActivity {
@@ -34,5 +36,11 @@ public class EnterTeamData extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void SubmitForm(View view) {
+        Intent intent = new Intent(this, EnterTeamData.class);
+        startActivity(intent);
+
     }
 }
